18548eaffa8b2cfc8a46e46ea43ad8de821fbf93
skalum <stkalum@gmail.com>
2018-10-17 16:37:11 -0400

    add bootstrap navbar, begin using react router to route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,96 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import {
+  Button,
+  Collapse,
+  Container,
+  Jumbotron,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 import AddressContainer from './containers/AddressContainer'
 
 class App extends Component {
+  state = {
+    isOpen: false,
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <AddressContainer />
-    );
+      <Router>
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">NYC Renter Toolkit</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/hpd/">Complaints, violations, etc.</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
 
-/*    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    ); */
+          <Route exact path="/" component={Home} />
+          <Route path="/hpd" component={Hpd} />
+        </div>
+      </Router>
+    );
   }
 }
+
+const Home = () => (
+  <div>
+    <Container>
+      <Jumbotron>
+        <h1 className="display-3">Weclcome to the NYC Renter Toolkit!</h1>
+        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-2" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+      </Jumbotron>
+    </Container>
+  </div>
+)
+
+const Hpd = () => (
+  <h2>Test Hpd</h2>
+)
 
 export default App;
